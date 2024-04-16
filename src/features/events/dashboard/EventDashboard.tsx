36a58f29df0164c639598e430977aecf474ca6bc
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-04-16 14:37:00 +0200

    End of section 4

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -8,23 +8,53 @@ import { AppEvent } from "../../../app/types/event";
 type Props = {
   formOpen: boolean
   setFormOpen: (value: boolean) => void
+  selectEvent: (event: AppEvent | null) => void
+  selectedEvent: AppEvent | null
 }
 
-export default function EventDashboard({formOpen, setFormOpen}: Props) {
+export default function EventDashboard({formOpen, setFormOpen, selectEvent, selectedEvent}: Props) {
   const [events, setEvents] = useState<AppEvent[]>([]);
+  
 
   useEffect(() => {
     setEvents(sampleData);
   }, []);
 
+  function addEvent(event: AppEvent){
+    setEvents(prevState => {
+      return [...prevState, event]
+    })
+  }
+
+  function updateEvent(updatedEvent: AppEvent){
+    setEvents(events.map(event => event.id === updatedEvent.id ? updatedEvent : event));
+    selectEvent(null);
+    setFormOpen(false);
+  }
+
+  function deleteEvent(eventId: string){
+    setEvents(events.filter(event => event.id !== eventId));
+  }
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList 
+          events={events} 
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
         {formOpen && 
-        <EventForm setFormOpen={setFormOpen}/> }
+        <EventForm 
+          setFormOpen={setFormOpen} 
+          updateEvent={updateEvent}
+          addEvent={addEvent}
+          selectedEvent={selectedEvent}
+          key={selectedEvent ?  selectedEvent.id : 'create'}
+        />
+         }
       </Grid.Column>
     </Grid>
   )
