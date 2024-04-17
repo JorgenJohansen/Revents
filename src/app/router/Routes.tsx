79a9670b14f88d87af3eac1478a1c4265555386a
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-04-17 15:13:10 +0200

    End of section 6

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -3,6 +3,9 @@ import App from "../layout/App";
 import EventDashboard from "../../features/events/dashboard/EventDashboard";
 import EventDetailedPage from "../../features/events/details/EventDetailedPage";
 import EventForm from "../../features/events/form/EventForm";
+import Scratch from "../../features/scratch/Scratch";
+
+
 
 export const router = createBrowserRouter([
     {
@@ -24,6 +27,10 @@ export const router = createBrowserRouter([
             {
                 path: '/createEvent',
                 element: <EventForm />
+            },
+            {
+                path: '/scratch',
+                element: <Scratch />
             },
         ]
     }
