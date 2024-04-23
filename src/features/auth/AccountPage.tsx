1c00ccb930bd70fc3e57463240dc517f541dd348
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2024-04-23 09:30:35 +0200

    End of section 9

--- src/features/auth/AccountPage.tsx
+++ src/features/auth/AccountPage.tsx
@@ -1,16 +1,37 @@
 import { FieldValues, useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
-import { Button, Form, Header, Icon, Segment } from "semantic-ui-react"
+import { Button, Form, Header, Icon, Label, Segment } from "semantic-ui-react"
 import { useAppSelector } from "../../app/store/store"
+import { useEffect } from "react";
+import { auth } from "../../app/config/firebase";
+import { updatePassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function AccountPage() {
     const {currentUser} = useAppSelector(state => state.auth);
-    const {register, handleSubmit, formState: {errors, isSubmitting, isValid}} = useForm({
+    const {register, handleSubmit, reset, getValues, setError, watch, trigger, formState: {errors, isSubmitting, isValid}} = useForm({
         mode: 'onTouched'
     })
+    const password1 = watch('password1');
+    const password2 = watch('password2');
 
-    function onSubmit(data: FieldValues){
-        console.log(data);
+    useEffect(() => {
+        if(password2) trigger('password2')
+    }, [password2, trigger, password1])
+
+
+    async function onSubmit(data: FieldValues){
+        try {
+            if(auth.currentUser){
+                await updatePassword(auth.currentUser, data.password1);
+                toast.success('Password updated successfully');
+                reset();
+            }
+        } catch (error: any) {
+            setError('root.serverError',{
+                type:'400', message: error.message
+            })
+        }
     }
   return (
     <Segment>
@@ -31,9 +52,25 @@ export default function AccountPage() {
                     type="password"
                     defaultValue=''
                     placeholder='Confirm Password'
-                    {...register('password2', {required: true})}
-                    error={errors.password2 && 'Confirm Password is required'}
+                    {...register('password2', {
+                        required: true,
+                        validate: {
+                            passwordMatch: value => (value === getValues().password1) || 'Passwords do not match'
+                        }
+                    })}
+                    error={
+                        errors.password2?.type === 'required' && 'Confirm Password is required' ||
+                        errors.password2?.type === 'passwordMatch' && errors.password2.message
+
+                    }
                 />
+                {errors.root && (
+                    <Label 
+                    basic color="red" 
+                    style={{display: 'block', marginBottom: 10}}
+                    content={errors.root.serverError.message}
+                    />
+                )}
                 <Button 
                     loading={isSubmitting} 
                     type="submit"
@@ -42,8 +79,6 @@ export default function AccountPage() {
                     positive
                     content="Update password"
                 />
-
-                
             </Form>
         </div>
         )}
